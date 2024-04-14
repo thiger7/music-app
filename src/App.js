95c1f9af824620cb30f050b57f0933395366f6b9
thiger7 <adf0423@gmail.com>
2024-04-14 14:55:30 +0900

    Spotify Client を refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import spotify from "./lib/spotify";
+
 export default function App() {
-  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID);
-  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_SECRET);
+  spotify.test();
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
