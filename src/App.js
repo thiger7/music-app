416ef887bd752b71fd60f2c9f595b6b43dfd6d80
thiger7 <adf0423@gmail.com>
2024-04-14 22:20:43 +0900

    クリックした曲を再生できるようにする

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SongList } from "./components/SongList";
 import spotify from "./lib/spotify";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [popularSongs, setPopularSongs] = useState([]);
+  const [isPlay, setIsPlay] = useState(false);
+  const [selectedSong, setSelectedSong] = useState();
+  const audioRef = useRef(null);
 
   useEffect(() => {
     fetchPopluarSongs();
@@ -19,6 +22,14 @@ export default function App() {
     setPopularSongs(popularSongs);
     setIsLoading(false);
   };
+
+  const handleSongSelected = async (song) => {
+    setSelectedSong(song);
+    audioRef.current.src = song.preview_url;
+    audioRef.current.play();
+    setIsPlay(true);
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
@@ -27,9 +38,14 @@ export default function App() {
         </header>
         <section>
           <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
-          <SongList isLoading={isLoading} songs={popularSongs} />
+          <SongList
+            isLoading={isLoading}
+            songs={popularSongs}
+            onSongSelected={handleSongSelected}
+          />
         </section>
       </main>
+      <audio ref={audioRef} />
     </div>
   );
 }
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -13,7 +13,7 @@ export function SongList(props) {
     <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
       {props.songs.map((song) => {
         return (
-          <div key={song.id} className="flex-none cursor-pointer ">
+          <div onClick={()=>props.onSongSelected(song)} key={song.id} className="flex-none cursor-pointer ">
             <img
               alt="thumbnail"
               src={song.album.images[0].url}
