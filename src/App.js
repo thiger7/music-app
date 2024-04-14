0b4bcb168fe74a6ebb8e9bb467646e55b6405661
thiger7 <adf0423@gmail.com>
2024-04-14 14:31:34 +0900

    環境変数にsecretを格納

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 export default function App() {
+  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_ID);
+  console.log(process.env.REACT_APP_SPOTIFY_CLIENT_SECRET);
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
